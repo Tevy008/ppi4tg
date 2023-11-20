9b85719263a49f67487b7adc9d1b3aad6cd55035
Tevy008 <110487893+Tevy008@users.noreply.github.com>
2023-11-20 16:22:47 +0300

    Исправил downloads.py

--- downloads.py
+++ downloads.py
@@ -1,11 +1,8 @@
 import requests
 import os
 
-
-def address_and_path(link,path,params=None):
+def download_images(link,path,params=None):
     response = requests.get(link,params)
     response.raise_for_status()
-    if not os.path.exists("images"):
-        os.mkdir("images")  
     with open(path, 'wb') as file:
         file.write(response.content)

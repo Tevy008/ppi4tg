9d2c0469afa555df3bdb56d885af664204f3c7ac
Tevy008 <110487893+Tevy008@users.noreply.github.com>
2023-11-29 16:23:35 +0300

    Перерешал урок, теперь можно получать фотографии с последнего запуска

--- spacex.py
+++ spacex.py
@@ -2,10 +2,11 @@ import requests
 from urllib.parse import urlparse,unquote
 from downloads import download_images
 from pathlib import Path
+import argparse
 
 
-def fetch_spacex_last_launch():
-    url = "https://api.spacexdata.com/v5/launches/5eb87d42ffd86e000604b384" 
+def fetch_spacex_last_launch(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}}" 
     response=requests.get(url)
     response.raise_for_status()
     links_image = response.json()["links"]["flickr"]["original"]
@@ -14,8 +15,11 @@ def fetch_spacex_last_launch():
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Программа для скачивания фотографий космического телеграма')
+    parser.add_argument('--id', dest="launch_id",default="5eb87d42ffd86e000604b384" help='укажите id нужного запуска spacex.py')
+    args = parser.parse_args()
     Path("images").mkdir(parents=True, exist_ok=True)
-    fetch_spacex_last_launch()
+    fetch_spacex_last_launch(args.lauch_id)
     
 
 
